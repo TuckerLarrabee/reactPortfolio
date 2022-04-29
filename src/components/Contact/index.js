dd8c47024a48ffa0d60caca1c1a0fd51c2b34395
tuckl <tuckerlarrabee13@gmail.com>
2022-04-29 18:40:49 -0400

    Contact added icons

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,39 @@
 import React from "react";
+import phone from "../../assets/phone-icon.jpg"
+import mail from "../../assets/mail-icon.jpg"
+import github from "../../assets/github-logo.png"
+import linkedin from "../../assets/linkedin.png"
+
 
 function Contact () {
 
 
     return (
-        <div>
-            
-        </div>
+        <section id="contact" className="contact">
+            <h2 className="contact-text">Contact Me:</h2>
+            <div className="contact-box">
+                <div className="phone">
+                    <img src={phone} alt="phone icon"></img>
+                    <h4>804.516.9034</h4>
+                </div>
+                <div className="email">
+                    <a href="mailto:tuckerlarrabee13@gmail.com">
+                        <img src={mail} alt="mail icon"></img>
+                        <h4>Email</h4>
+                    </a>
+                </div>
+                <div className="github">
+                    <a href="https://github.com/TuckerLarrabee" target={"_blank"}>
+                        <img src={github} alt="github logo"></img>
+                        <h4>GitHub</h4>
+                    </a>
+                </div>
+                <div className="linkedin">
+                    <img src={linkedin} alt="linkedin logo"></img>
+                    <h4>LinkedIn</h4>
+                </div>
+            </div>
+        </section>
     )
 }
 
