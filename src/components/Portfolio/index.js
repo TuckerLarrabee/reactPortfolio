781319f7140b95f54ee3c354dceb4a1fd75ae39f
tuckl <tuckerlarrabee13@gmail.com>
2022-04-29 18:41:56 -0400

    projects added to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,78 @@
 import React from "react";
+import crypto from "../../assets/cryptohub.png"
+import music from "../../assets/MusicYouMayLike.png"
+import weather from "../../assets/WeatherInfoToday.png"
+import gitItDone from "../../assets/GitItDone.png"
 
 function Portfolio () {
 
 
     return (
-        <div>
-            
-        </div>
+        <section id="portfolio">
+            <h2 className="project-text">Current Projects:</h2>
+            <div className="row">
+                <div className="main-project">
+                    <a href="https://secret-cliffs-96796.herokuapp.com/login" target={"_blank"}>
+                        <img src={crypto}></img>
+                    </a>
+                    <div className="main-project-text">
+                        <a href="https://secret-cliffs-96796.herokuapp.com/login" target={"_blank"}>
+                            <p >Node/Express/Handlebars</p>
+                            <h3>CryptoHub</h3>
+                        </a>
+                        {/* <a>
+                            Link to Github
+                        </a> */}
+                    </div>
+                </div>
+            </div>
+            <div className="column">
+                <div className="project2">
+                    <a href="https://tuckerlarrabee.github.io/Music-You-May-Like/" target={"_blank"}>
+                        <img style={{margin:0}} src={music}></img>
+                    </a>
+                    <div>
+                        <a href="https://tuckerlarrabee.github.io/Music-You-May-Like/" target={"_blank"}>
+                            <p >HTML/CSS/JS</p>
+                            <h3>Music You May Like</h3>
+                        </a>
+                    </div>
+                </div>
+                <div className="project3">
+                    <a href="https://tuckerlarrabee.github.io/WeatherInfoToday/" target={"_blank"}>
+                        <img style={{margin:0}} src={weather}></img>
+                    </a>
+                    <div>
+                        <a href="https://tuckerlarrabee.github.io/WeatherInfoToday/" target={"_blank"}>
+                            <p >HTML/CSS/JS</p>
+                            <h3>Weather Info Today</h3>
+                        </a>
+                    </div>
+                </div>
+                <div className="project4">
+                    <a href="https://tuckerlarrabee.github.io/git-it-done/index.html" target={"_blank"}>
+                        <img style={{margin:0}} src={gitItDone}></img>
+                    </a>
+                    <div>
+                        <a href="https://tuckerlarrabee.github.io/git-it-done/index.html" target={"_blank"}>
+                            <p >HTML/CSS/JS</p>
+                            <h3>Git it Done</h3>
+                        </a>
+                    </div>
+                </div>
+                {/* <div className="project5">
+                    <a href="https://tuckerlarrabee.github.io/Music-You-May-Like/" target={"_blank"}>
+                        <img style={{margin:0}} src={music}></img>
+                    </a>
+                    <div>
+                        <a href="https://tuckerlarrabee.github.io/Music-You-May-Like/" target={"_blank"}>
+                            <p >HTML/CSS/JS</p>
+                            <h3>Music You May Like</h3>
+                        </a>
+                    </div>
+                </div> */}
+            </div>
+        </section>
     )
 }
 
