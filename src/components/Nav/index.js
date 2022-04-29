d9607c500b336a7ef6d9dda24a6f19e12b6b449f
tuckl <tuckerlarrabee13@gmail.com>
2022-04-29 18:41:44 -0400

    Nav completed with hyperlink

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,32 +1,43 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+    contactSelected,
+    setContactSelected,
+  } = props;
 
+  console.log(currentCategory);
 
-function Nav (props) {
-    const {
-        categories = [],
-        setCurrentCategory,
-        currentCategory,
-        contactSelected,
-        setContactSelected
-    } = props;
-
-console.log(currentCategory)
-
-    return (
-        <nav>
-            <ul className='flex-row' style={{margin: 0}}>
-                {categories.map((category) => (
-                    <li
-                        className={`mx-2 ${currentCategory.name === category.name && !contactSelected && 'navActive'}`} key={category.name}>
-                            <a href={`#`+category.name} onClick={() => {setCurrentCategory(category); setContactSelected(false);}}>
-                                {category.name}
-                            </a>
-                        </li>
-                ))}            
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <ul className="flex-row" style={{ margin: 0 }}>
+        {categories.map((category) => (
+          <li
+            className={`mx-2 ${
+              currentCategory.name === category.name &&
+              !contactSelected &&
+              "navActive"
+            }`}
+            key={category.name}
+          >
+            <a
+              href={"#" + category.name.toLowerCase().replace(" ", "-")}
+              onClick={() => {
+                console.log(category)
+                setCurrentCategory(category);
+                setContactSelected(false);
+              }}
+            >
+              {category.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
