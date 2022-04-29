13b9c786fe2c8cc3594305056eb8cfcf816bb29c
tuckl <tuckerlarrabee13@gmail.com>
2022-04-29 18:40:41 -0400

    About Me completed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 
+
 function About () {
 
 
     return (
-        <section id="about-me">
-            <div className="about-me-img"></div>
+        <section id="about-me" >
+            <div className="about-me-img">
+                <p>I am a Full-stack web developer utilizing functional, agile, 
+            and SDLC knowledge from 5 years of experience as a Business 
+            Analyst for multiple Insurtech software suite providers. Recently 
+            earned a certificate in full stack web development from the 
+            University of Richmond, with skills in HTML5/CSS3, JavaScript, 
+            Node.js, Express.js, React.js, and responsive web design.
+                </p>
+                <div className="about-me-headshot">
+                    <h2 className="about-me-text">About Me</h2>
+                    <img className="headshot"></img>
+                </div>
+            </div>
         </section>
     )
 }
