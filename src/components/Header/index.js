9440c9a164ad13940ed5459035dd0cb2d6b89e17
tuckl <tuckerlarrabee13@gmail.com>
2022-04-29 18:41:21 -0400

    Header completed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,35 @@
 import React, { useState } from "react";
 import Nav from "../Nav";
 
-function Header () {
-    const [categories] = useState([
-        { name: 'About Me'},
-        { name: 'Portfolio'},
-        { name: 'Contact'},
-        { name: 'Resume'}
-    ])
+function Header() {
+  const [categories] = useState([
+    { name: "About Me" },
+    { name: "Portfolio" },
+    { name: "Contact" },
+    { name: "Resume" },
+  ]);
 
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
-    const [currentCategory, setCurrentCategory] = useState(categories[0])
+  const [contactSelected, setContactSelected] = useState(false);
 
-    const [contactSelected, setContactSelected] = useState(false)
-
-    console.log(currentCategory)
-    return (
-        <header className="nav-bar">
-            <h2>
-                <a href="/">
-                    <span id="myName">Tucker Larrabee</span>
-                </a>
-            </h2>
-            <Nav
-            categories={categories}
-            setCurrentCategory={setCurrentCategory}
-            currentCategory={currentCategory}
-            contactSelected={contactSelected}
-            setContactSelected={setContactSelected}
-            ></Nav>
-        </header>
-    )
+  console.log(currentCategory);
+  return (
+    <header className="nav-bar">
+      <h2>
+        <a href="/">
+          <span id="myName">Tucker Larrabee</span>
+        </a>
+      </h2>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+    </header>
+  );
 }
 
 export default Header;
