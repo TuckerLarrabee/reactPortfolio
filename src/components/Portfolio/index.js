c4f3cf23a6b009f59911d269eda993438dbcae7f
tuckl <tuckerlarrabee13@gmail.com>
2022-04-30 13:33:37 -0400

    finishing touches

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import crypto from "../../assets/cryptohub.png"
 import music from "../../assets/MusicYouMayLike.png"
 import weather from "../../assets/WeatherInfoToday.png"
-import gitItDone from "../../assets/GitItDone.png"
+import notes from "../../assets/note-taker.png"
 import techblog from "../../assets/tech-blog.png"
+import regex from "../../assets/regex.jpeg"
+import github from "../../assets/github-logo.png"
 
 function Portfolio () {
 
@@ -21,9 +23,9 @@ function Portfolio () {
                             <p >Node/Express/Handlebars</p>
                             <h3>CryptoHub</h3>
                         </a>
-                        {/* <a>
-                            Link to Github
-                        </a> */}
+                        <a className="project-text-link" href="https://github.com/TuckerLarrabee/CryptoHub" target={"_blank"}>
+                            Link to Github 
+                        </a>
                     </div>
                 </div>
             </div>
@@ -37,6 +39,9 @@ function Portfolio () {
                             <p >HTML/CSS/JS</p>
                             <h3>Music You May Like</h3>
                         </a>
+                        <a className="project-text-link" href="https://github.com/TuckerLarrabee/Music-You-May-Like" target={"_blank"}>
+                            Link to Github 
+                        </a>
                     </div>
                 </div>
                 <div className="project3">
@@ -47,6 +52,9 @@ function Portfolio () {
                         <a href="https://tuckerlarrabee.github.io/WeatherInfoToday/" target={"_blank"}>
                             <p >Server Side APIs/JS</p>
                             <h3>Weather Info Today</h3>
+                        </a>
+                        <a className="project-text-link" href="https://github.com/TuckerLarrabee/WeatherInfoToday" target={"_blank"}>
+                            Link to Github 
                         </a>
                     </div>
                 </div>
@@ -59,19 +67,39 @@ function Portfolio () {
                             <p >MVC/MySQL2/Express</p>
                             <h3>Tech Blog</h3>
                         </a>
-                    </div>
-                </div>
-                {/* <div className="project5">
-                    <a href="https://tuckerlarrabee.github.io/Music-You-May-Like/" target={"_blank"}>
-                        <img style={{margin:0}} src={music}></img>
-                    </a>
-                    <div>
-                        <a href="https://tuckerlarrabee.github.io/Music-You-May-Like/" target={"_blank"}>
-                            <p >HTML/CSS/JS</p>
-                            <h3>Music You May Like</h3>
+                        <a className="project-text-link" href="https://github.com/TuckerLarrabee/TechBlog_MVC" target={"_blank"}>
+                            Link to Github 
                         </a>
                     </div>
-                </div> */}
+                </div>
+                <div className="project5">
+                    <a href="https://gist.github.com/TuckerLarrabee/b1c2b7221ee783e718f28ac8fda86da8" target={"_blank"}>
+                        <img style={{margin:0}} src={regex}></img>
+                    </a>
+                    <div>
+                        <a href="https://gist.github.com/TuckerLarrabee/b1c2b7221ee783e718f28ac8fda86da8" target={"_blank"}>
+                            <p >Regex</p>
+                            <h3>Regex Email Validation</h3>
+                        </a>
+                        <a className="project-text-link" href="https://gist.github.com/TuckerLarrabee/b1c2b7221ee783e718f28ac8fda86da8" target={"_blank"}>
+                            Link to Github 
+                        </a>
+                    </div>
+                </div>
+                <div className="project6">
+                    <a href="https://notesfordays123.herokuapp.com/notes" target={"_blank"}>
+                        <img style={{margin:0}} src={notes}></img>
+                    </a>
+                    <div>
+                        <a href="https://notesfordays123.herokuapp.com/notes" target={"_blank"}>
+                            <p >Node/Express</p>
+                            <h3>Note Taker</h3>
+                        </a>
+                        <a className="project-text-link" href="https://github.com/TuckerLarrabee/NotesForDays" target={"_blank"}>
+                            Link to Github 
+                        </a>
+                    </div>
+                </div>
             </div>
         </section>
     )
