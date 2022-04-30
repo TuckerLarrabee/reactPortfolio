1da7539576697e585e664cbf22f8f0d7679e33bb
tuckl <tuckerlarrabee13@gmail.com>
2022-04-30 12:40:51 -0400

    fixed Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,12 @@
 import React from "react";
+import avatar from "../../assets/sticker.png"
 
 function Hero () {
 
 
     return (
         <section className="hero">
+            <img className="avatar" src={avatar}></img>
             <p className="hero-text">Web and App Developer</p>
         </section>
     )
