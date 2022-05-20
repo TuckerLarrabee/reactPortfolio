8be05fd49840f4c3b7eae1e59e3dd79371a94a24
tuckl <tuckerlarrabee13@gmail.com>
2022-05-19 21:36:58 -0400

    last change

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,23 @@
 import React from "react";
-import avatar from "../../assets/sticker.png"
+import avatar from "../../assets/sticker.png";
 
-function Hero () {
-
-
-    return (
-        <section className="hero">
-            <div style={{"width": "80vw", "display": "flex", "flex-wrap": "wrap",
-    "align-items": 'center',
-    "justify-content": "center"}}>
-                <img className="avatar" src={avatar}></img>
-                <p className="hero-text">Web and App Developer</p>
-            </div>
-        </section>
-    )
+function Hero() {
+  return (
+    <section className="hero">
+      <div
+        style={{
+          width: "80vw",
+          display: "flex",
+          "flex-wrap": "wrap",
+          "align-items": "center",
+          "justify-content": "center",
+        }}
+      >
+        <img className="avatar" src={avatar}></img>
+        <p className="hero-text">Web and App Developer</p>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
